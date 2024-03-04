9a4a28a400b7d7b7ae9371f29cdab90a3b94aee0
luciablascogallego <lublas01@ucm.es>
2024-03-04 21:25:32 +0100

    Update index.php

--- PR2/index.php
+++ PR2/index.php
@@ -1,26 +1,28 @@
+<?php session_start() ?>
 <!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Portada</title>
-</head>
 
-<body>
+<html lang = "es">
+	<head>
+		<link rel="stylesheet" type="text/css" href="estilo.css" />
+		<meta charset="utf-8">
+		<title>Portada</title>
+	</head>
 
-<div id="contenedor"> <!-- Inicio del contenedor -->
+	<body>
 
-	<?php
-		require_once 'vistas/cabecera.php';
-		require_once 'vistas/sidebarIzq.php';
-	?>
-	<main>
-	  <article>
-		<h1>Página principal</h1>
-	  </article>
-	</main>
-	
-</div> <!-- Fin del contenedor -->
+		<div id="contenedor"> <!-- Inicio del contenedor -->
 
-</body>
+			<?php
+				require_once 'vistas/cabecera.php';
+				require_once 'vistas/sidebarIzq.php';
+			?>
+			<main>
+			<article>
+				<h1>Página principal</h1>
+			</article>
+			</main>
+			
+		</div> <!-- Fin del contenedor -->
+
+	</body>
 </html>
